80ccac78beb260d4809957fe1e4bfa21672545d5
Per0na <hola@per0na.es>
2024-02-18 19:36:21 +0000

    Add API endpoint to get a pokemon by id

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/Per0na/golang-pokemon-api/models"
 )
@@ -20,10 +21,40 @@ func main() {
 	server := http.NewServeMux()
 
 	// Find all pokemons from the pokemon array
-	server.HandleFunc("/v1/pokemons", func(w http.ResponseWriter, r *http.Request) {
+	server.HandleFunc("GET /v1/pokemons", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
 		json.NewEncoder(w).Encode(pokemons)
+	})
+
+	// Find a pokemon by id
+	server.HandleFunc("GET /v1/pokemons/{id}", func(w http.ResponseWriter, r *http.Request) {
+		idStr := r.PathValue("id")
+		// Convert the id to int
+		id, err := strconv.Atoi(idStr)
+		if err != nil {
+			http.Error(w, "Invalid id", http.StatusBadRequest)
+			return
+		}
+
+		// Find the pokemon by id
+		var pokemon models.Pokemon
+		for _, p := range pokemons {
+			if p.Id == id {
+				pokemon = p
+				break
+			}
+		}
+
+		// If the pokemon is not found
+		if pokemon.Id == 0 {
+			http.Error(w, "Pokemon not found", http.StatusNotFound)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(pokemon)
 	})
 
 	// Run the server
